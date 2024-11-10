b9b4e372d66d594370f59181f506ecede0ab78bc
Yazan Alhalabi <yazanalhalabi1@gmail.com>
2024-11-11 00:37:21 +0200

    init proj

---
+++ src/test/java/fs19/java/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package fs19.java.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
