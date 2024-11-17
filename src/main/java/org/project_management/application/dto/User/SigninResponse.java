176a4e27949155d5a01650a5ea4c24998ceb4a8f
zemikyafu <zemikfcb@gmail.com>
2024-11-17 13:33:03 +0200

    signin dto created

---
+++ src/main/java/org/project_management/application/dto/User/SigninResponse.java
@@ -0,0 +1,18 @@
+package org.project_management.application.dto.User;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.project_management.domain.entities.user.Status;
+import java.util.UUID;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class SigninResponse {
+    private String token;
+    private UUID id;
+    private String name;
+}
