9f17d687f658450ab0f6efd1318d2d94c8070452
zemikyafu <zemikfcb@gmail.com>
2024-11-17 13:36:52 +0200

    create secuirty config,Jwt handlers,and authentication and access denied handlers

---
+++ src/main/java/org/project_management/presentation/config/JwtHelper.java
@@ -0,0 +1,75 @@
+package org.project_management.presentation.config;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+import java.util.function.Function;
+
+    @Component
+    public class JwtHelper {
+        @Value("${jwt.secret}")
+        private String JWT_SECRET;
+
+
+        public String extractUserEmail(String token) {
+            return extractClaim(token, Claims::getSubject);
+        }
+
+        public Date extractIssuedAt(String token) {
+            return extractClaim(token, Claims::getIssuedAt);
+        }
+
+        public boolean isTokenValid(String token, UserDetails userDetails) {
+            final String userEmail = extractUserEmail(token);
+            final Date tokenExpirationDate = extractClaim(token, Claims::getExpiration);
+
+            boolean usernameMatch = Objects.equals(userEmail, userDetails.getUsername());
+            boolean tokenIsExpired = tokenExpirationDate.before(new Date(System.currentTimeMillis()));
+
+            return usernameMatch && !tokenIsExpired;
+        }
+
+        public String generateToken(UserDetails userDetails) {
+            return this.generateToken(new HashMap<>(), userDetails);
+        }
+
+        public String generateToken(Map<String, Object> extraClaims, UserDetails userDetails) {
+            return Jwts
+                    .builder()
+                    .claims(extraClaims)
+                    .subject(userDetails.getUsername())
+                    .issuedAt(new Date(System.currentTimeMillis()))
+                    .expiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 24 * 14))
+                    .signWith(getSignInKey())
+                    .compact();
+        }
+
+        public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
+            final Claims claims = extractAllClaims(token);
+            return claimsResolver.apply(claims);
+        }
+
+        private Claims extractAllClaims(String token) {
+            return Jwts
+                    .parser()
+                    .verifyWith(getSignInKey())
+                    .build()
+                    .parseSignedClaims(token)
+                    .getPayload();
+        }
+
+        private SecretKey getSignInKey() {
+            byte[] keyBytes = Decoders.BASE64.decode(JWT_SECRET);
+            return Keys.hmacShaKeyFor(keyBytes);
+        }
+}
---
+++ src/main/java/org/project_management/presentation/config/SecurityConfig.java
@@ -0,0 +1,60 @@
+package org.project_management.presentation.config;
+
+import org.project_management.application.services.UserDetailsServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    private final UserDetailsServiceImpl userDetailService;
+    private final JwtAuthFilter jwtAuthFilter;
+    private final CustomAuthenticationEntryPoint authenticationHandler;
+    private final CustomAccessDeniedHandler accessDeniedHandler;
+
+    public SecurityConfig(UserDetailsServiceImpl userDetailService, JwtAuthFilter jwtAuthFilter,
+                          CustomAuthenticationEntryPoint authenticationHandler, CustomAccessDeniedHandler accessDeniedHandler) {
+        this.userDetailService = userDetailService;
+        this.jwtAuthFilter = jwtAuthFilter;
+        this.authenticationHandler = authenticationHandler;
+        this.accessDeniedHandler = accessDeniedHandler;
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.cors().and().csrf().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
+                .authorizeHttpRequests()
+                .requestMatchers("/api/v1/auth/**").permitAll()
+                .anyRequest().authenticated().and()
+                .exceptionHandling()
+                .authenticationEntryPoint(authenticationHandler)
+                .accessDeniedHandler(accessDeniedHandler);;
+
+        http.addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
+                .authenticationManager(authenticationManager(http));
+
+        return http.build();
+    }
+
+    @Bean
+    AuthenticationManager authenticationManager(HttpSecurity http) throws Exception {
+        AuthenticationManagerBuilder authenticationManagerBuilder = http.getSharedObject(AuthenticationManagerBuilder.class);
+        authenticationManagerBuilder.userDetailsService(userDetailService).passwordEncoder(passwordEncoder());
+        return authenticationManagerBuilder.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
