05df4d43a80fa79a107c429afaaa472a72df7db2
zemikyafu <zemikfcb@gmail.com>
2024-11-18 21:03:16 +0200

    Implement JPA repository for User entity

--- src/main/java/org/project_management/infrastructure/repositoryImpl/UserRepositoryImpl.java
+++ src/main/java/org/project_management/infrastructure/repositoryImpl/UserRepositoryImpl.java
@@ -1,42 +1,35 @@
-package org.project_management.infrastructure.repositories;
+package org.project_management.infrastructure.repositoryImpl;
 
-import jakarta.persistence.EntityManager;
-import jakarta.persistence.PersistenceContext;
-import jakarta.persistence.Query;
-import jakarta.persistence.TypedQuery;
-import jakarta.transaction.Transactional;
 import org.project_management.application.exceptions.BadRequestException;
 import org.project_management.application.exceptions.ResourceNotFoundException;
 import org.project_management.application.exceptions.UnableToDeleteResourceException;
 import org.project_management.application.exceptions.UnableToSaveResourceException;
 import org.project_management.domain.abstractions.UserRepository;
 import org.project_management.domain.entities.user.User;
+import org.project_management.infrastructure.jpa_repositories.JpaUserRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
-import java.util.stream.Collectors;
 
 @Repository
 public class UserRepositoryImpl implements UserRepository {
-    @PersistenceContext
-    private EntityManager entityManager;
+    private final JpaUserRepository jpaUserRepository;
+    public UserRepositoryImpl(JpaUserRepository jpaUserRepository) {
+        this.jpaUserRepository = jpaUserRepository;
+    }
 
     @Override
-    @Transactional
     public User save(User user) {
+
         if (user.getName() == null || user.getEmail() == null || user.getPassword() == null) {
             throw new BadRequestException("Name, email and password are required");
         }
         try {
-            UserEntity userEntity = new UserEntity(user.getName(), user.getEmail(), user.getPassword());
-            entityManager.persist(userEntity);
-
-            User newUser = new User(userEntity.getId(),userEntity.getName(), userEntity.getEmail(), userEntity.getPassword(), userEntity.getStatus());
-            newUser.setId(userEntity.getId());
-
-            return newUser;
+            return jpaUserRepository.save(user);
         } catch (Exception e) {
             e.printStackTrace();
             throw new UnableToSaveResourceException("Unable to save user");
@@ -45,113 +38,67 @@ public class UserRepositoryImpl implements UserRepository {
 
     @Override
     public Optional<User> findById(UUID id) {
-        UserEntity userEntity = entityManager.find(UserEntity.class, id);
-        if (userEntity == null) {
-            throw new ResourceNotFoundException("User not found with id: " + id.toString());
-        }
-
-        User user = new User(userEntity.getId(), userEntity.getName(), userEntity.getEmail(), userEntity.getPassword(), userEntity.getStatus());
-
-        return Optional.of(user);
+        return jpaUserRepository.findById(id)
+                .or(() -> {
+                    throw new ResourceNotFoundException("User not found with id: " + id.toString());
+                });
     }
 
+    @Override
+    public Optional<User> findByEmail(@Param("email") String email) {
+        return jpaUserRepository.findByEmail(email);
+    }
     @Override
     public List<User> findAll() {
-        TypedQuery<UserEntity> query = entityManager.createQuery("SELECT u FROM UserEntity u", UserEntity.class);
-        List<UserEntity> userEntities = query.getResultList();
-
-        return userEntities.stream().map((userEntity) -> {
-            User user = new User(userEntity.getId(), userEntity.getName(), userEntity.getEmail(), userEntity.getPassword(), userEntity.getStatus());
-            return user;
-        }).collect(Collectors.toList());
+        return jpaUserRepository.findAll();
     }
 
-    @Override
-    public Optional<User> findByEmail(String email) {
-        if (email == null) {
-            throw new BadRequestException("Email is required");
-        }
-
-        TypedQuery<UserEntity> query = entityManager.createQuery("SELECT u FROM UserEntity u WHERE u.email = :email", UserEntity.class);
-        query.setParameter("email", email);
-        List<UserEntity> userEntities = query.getResultList();
-
-        if (userEntities.isEmpty()) {
-            return Optional.empty();
-        }
-
-        UserEntity userEntity = userEntities.get(0);
-        User user = new User(userEntity.getId(), userEntity.getName(), userEntity.getEmail(), userEntity.getPassword(), userEntity.getStatus());
-
-        return Optional.of(user);
-    }
-
-    @Transactional
     @Override
     public User updateUser(User user) {
-        UserEntity userEntity = entityManager.find(UserEntity.class, user.getId());
-
-        if (userEntity == null) {
-            throw new ResourceNotFoundException("User not found");
-        }
+        User updateUser = jpaUserRepository.findById(user.getId())
+                .orElseThrow(() -> new ResourceNotFoundException("User not found"));
 
         if (user.getName() == null || user.getEmail() == null || user.getPassword() == null || user.getStatus() == null) {
             throw new BadRequestException("Name, email, password, and status are required");
         }
-        userEntity.setName(user.getName());
-        userEntity.setEmail(user.getEmail());
-        userEntity.setPassword(user.getPassword());
-        userEntity.setStatus(user.getStatus());
+
+        updateUser.setName(user.getName());
+        updateUser.setEmail(user.getEmail());
+        updateUser.setPassword(user.getPassword());
+        updateUser.setStatus(user.getStatus());
 
         try {
-            entityManager.merge(userEntity);
+            return jpaUserRepository.save(updateUser);
         } catch (Exception e) {
-            e.printStackTrace();
             throw new UnableToSaveResourceException("Unable to fully update user");
         }
-        return user;
     }
+    @Override
+    public User updateNameOrEmail(User user) {
+        User updateUser = jpaUserRepository.findById(user.getId())
+                .orElseThrow(() -> new ResourceNotFoundException("User not found"));
 
-     @Transactional
-     public User updateNameOrEmail(User user) {
-         UserEntity userEntity = entityManager.find(UserEntity.class, user.getId());
-
-         if (userEntity == null) {
-             throw new ResourceNotFoundException("User not found");
-         }
-
-         // Only update fields that are not null (for partial updates)
-         if (user.getName() != null) {
-             userEntity.setName(user.getName());
-         }
-         if (user.getEmail() != null) {
-             userEntity.setEmail(user.getEmail());
-         }
-
-         try {
-             entityManager.merge(userEntity);
-         } catch (Exception e) {
-             e.printStackTrace();
-             throw new UnableToSaveResourceException("Unable to partially update user");
-         }
-         user.setStatus(userEntity.getStatus());
-         return user;
+        if (user.getName() != null) {
+            updateUser.setName(user.getName());
+        }
+        if (user.getEmail() != null) {
+            updateUser.setEmail(user.getEmail());
         }
 
-    @Transactional
+        try {
+            return jpaUserRepository.save(updateUser);
+        } catch (Exception e) {
+            throw new UnableToSaveResourceException("Unable to partially update user");
+        }
+    }
     @Override
     public void deleteUser(UUID id) {
-        try {
-            UserEntity userEntity = entityManager.find(UserEntity.class, id);
-            if (userEntity == null) {
-                throw new ResourceNotFoundException("User not found with id: " + id.toString());
-            }
-            Query query = entityManager.createQuery("DELETE FROM UserEntity u WHERE u.id = :id");
-            query.setParameter("id", id);
+        User user = jpaUserRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("User not found with id: " + id.toString()));
 
-            query.executeUpdate();
+        try {
+            jpaUserRepository.delete(user);
         } catch (Exception e) {
-            e.printStackTrace();
             throw new UnableToDeleteResourceException("Unable to delete user");
         }
     }
