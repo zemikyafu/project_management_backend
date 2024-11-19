f26e9122ba43751ab12417f16a72767cb77a67f6
asharmehmood-7 <asharmehmood056@gmail.com>
2024-11-19 19:42:41 +0200

    added repositories abstraction

---
+++ src/main/java/org/project_management/domain/abstractions/TaskRepository.java
@@ -0,0 +1,19 @@
+package org.project_management.domain.abstractions;
+
+import org.project_management.domain.entities.task.Task;
+import org.project_management.domain.entities.task.TaskStatus;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+public interface TaskRepository {
+    Task save(Task task);
+    List<Task> findByProjectId(UUID projectId);
+    Optional<Task> findByIdAndProjectId(UUID taskId, UUID projectId);
+    Task update(Task task);
+    List<Task> findByStatus(TaskStatus status);
+    List<Task> findByAssignee(UUID assigneeId);
+    List<Task> findByDeadlineAtBefore(java.util.Date deadlineDate);
+    void deleteByIdAndProjectId(UUID taskId, UUID projectId);
+}
