8e6ca822e363c210138d7949e891056fa40fe610
Jere Stigell <jj.stigell@gmail.com>
2024-11-20 17:35:28 +0900

    role repo

--- src/main/java/org/project_management/domain/abstractions/RoleRepository.java
+++ src/main/java/org/project_management/domain/abstractions/RoleRepository.java
@@ -1,4 +1,16 @@
 package org.project_management.domain.abstractions;
 
+import org.project_management.domain.entities.role.Role;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
 public interface RoleRepository {
+    Role save(Role role);
+    Optional<Role> findById(UUID id);
+    Optional<Role> findByName(String name);
+    Role update(Role role);
+    void delete(UUID id);
+    List<Role> findAll();
 }
