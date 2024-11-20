18393de130780536a323362ab80fe1de05630ecd
Jere Stigell <jj.stigell@gmail.com>
2024-11-20 17:42:44 +0900

    add companyUserRepo and rolePremissionRepo

--- src/main/java/org/project_management/domain/abstractions/CompanyUserRepository.java
+++ src/main/java/org/project_management/domain/abstractions/CompanyUserRepository.java
@@ -1,4 +1,15 @@
 package org.project_management.domain.abstractions;
 
+import org.project_management.domain.entities.company.CompanyUser;
+import org.project_management.domain.entities.company.CompanyUserId;
+
+import java.util.List;
+import java.util.Optional;
+
 public interface CompanyUserRepository {
+    CompanyUser save(CompanyUser companyUser);
+    Optional<CompanyUser> findById(CompanyUserId id);
+    CompanyUser update(CompanyUser companyUser);
+    void delete(CompanyUser companyUser);
+    List<CompanyUser> findAll();
 }
--- src/main/java/org/project_management/domain/abstractions/RolePermissionRepository.java
+++ src/main/java/org/project_management/domain/abstractions/RolePermissionRepository.java
@@ -1,4 +1,15 @@
 package org.project_management.domain.abstractions;
 
-public interface RolePermission {
+import org.project_management.domain.entities.role.RolePermission;
+import org.project_management.domain.entities.role.RolePermissionId;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface RolePermissionRepository {
+    RolePermission save(RolePermission rolePermission);
+    Optional<RolePermission> findById(RolePermissionId id);
+    RolePermission update(RolePermission rolePermission);
+    void delete(RolePermission rolePermission);
+    List<RolePermission> findAll();
 }
