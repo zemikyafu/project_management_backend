b29c1a048c5ff74f3be0fd037739c600d600ef6e
Jere Stigell <jj.stigell@gmail.com>
2024-11-21 12:40:39 +0900

    update delete methods

--- src/main/java/org/project_management/domain/abstractions/CompanyUserRepository.java
+++ src/main/java/org/project_management/domain/abstractions/CompanyUserRepository.java
@@ -5,11 +5,12 @@ import org.project_management.domain.entities.company.CompanyUserId;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.UUID;
 
 public interface CompanyUserRepository {
     CompanyUser save(CompanyUser companyUser);
     Optional<CompanyUser> findById(CompanyUserId id);
     CompanyUser update(CompanyUser companyUser);
-    void delete(CompanyUser companyUser);
+    void delete(UUID userId, UUID companyId);
     List<CompanyUser> findAll();
 }
--- src/main/java/org/project_management/domain/abstractions/RolePermissionRepository.java
+++ src/main/java/org/project_management/domain/abstractions/RolePermissionRepository.java
@@ -5,11 +5,12 @@ import org.project_management.domain.entities.role.RolePermissionId;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.UUID;
 
 public interface RolePermissionRepository {
     RolePermission save(RolePermission rolePermission);
     Optional<RolePermission> findById(RolePermissionId id);
     RolePermission update(RolePermission rolePermission);
-    void delete(RolePermission rolePermission);
+    void delete(UUID roleId, UUID permissionId);
     List<RolePermission> findAll();
 }
