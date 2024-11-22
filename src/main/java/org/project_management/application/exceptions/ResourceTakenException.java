b6248819fa2a4b3de8f685a74d469e81817510e3
Jere Stigell <jj.stigell@gmail.com>
2024-11-23 00:06:53 +0900

    ResourceTakenException

--- src/main/java/org/project_management/application/exceptions/ResourceTakenException.java
+++ src/main/java/org/project_management/application/exceptions/ResourceTakenException.java
@@ -1,4 +1,7 @@
 package org.project_management.application.exceptions;
 
-public class ResourceTakenException {
+public class ResourceTakenException extends RuntimeException {
+    public ResourceTakenException(String message) {
+        super(message);
+    }
 }
