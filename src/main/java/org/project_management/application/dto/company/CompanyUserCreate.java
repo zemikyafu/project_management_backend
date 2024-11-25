0a1ce571b831be8a0b9411d404694021e945d028
zemikyafu <zemikfcb@gmail.com>
2024-11-25 22:38:14 +0200

    create dto

---
+++ src/main/java/org/project_management/application/dto/company/CompanyUserCreate.java
@@ -0,0 +1,17 @@
+package org.project_management.application.dto.company;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.UUID;
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class CompanyUserCreate {
+    private UUID userId;
+    private UUID companyId;
+
+}
