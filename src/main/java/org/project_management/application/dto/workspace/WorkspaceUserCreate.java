157c79e50ff697908d9442901546cee21f29561a
asharmehmood-7 <asharmehmood056@gmail.com>
2024-11-26 05:07:04 +0200

    added workspaceuser controller

--- src/main/java/org/project_management/application/dto/workspace/WorkspaceUserCreate.java
+++ src/main/java/org/project_management/application/dto/workspace/WorkspaceUserCreate.java
@@ -3,14 +3,17 @@ package org.project_management.application.dto.workspace;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.util.UUID;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
+@Setter
 public class WorkspaceUserCreate {
     private UUID userId;
     private UUID roleId;
     private UUID workspaceId;
+
 }
