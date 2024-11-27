1fbbfb1da8c7684c661f0a28045094815aa71f75
zemikyafu <zemikfcb@gmail.com>
2024-11-27 22:53:12 +0200

    update service that have missing @Service anotations

--- src/main/java/org/project_management/application/services/User/UserServiceImpl.java
+++ src/main/java/org/project_management/application/services/User/UserServiceImpl.java
@@ -3,6 +3,7 @@ package org.project_management.application.services.User;
 import org.project_management.domain.abstractions.UserRepository;
 import org.project_management.domain.entities.user.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -12,10 +13,12 @@ import java.util.UUID;
 @Service
 public class UserServiceImpl implements UserService {
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
     @Autowired
-    public UserServiceImpl(UserRepository userRepository) {
+    public UserServiceImpl(UserRepository userRepository, PasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     @Override
@@ -25,7 +28,8 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public User updateUser(User user) {
-      return userRepository.updateUser(user);
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        return userRepository.updateUser(user);
     }
 
     @Override
--- src/main/java/org/project_management/application/services/workspace/WorkspaceUserServiceImpl.java
+++ src/main/java/org/project_management/application/services/workspace/WorkspaceUserServiceImpl.java
@@ -15,11 +15,12 @@ import org.project_management.domain.entities.workspace.Workspace;
 import org.project_management.domain.entities.workspace.WorkspaceUser;
 import org.project_management.presentation.config.JwtAuthFilter;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
-
+@Service
 public class WorkspaceUserServiceImpl implements WorkspaceUserService{
     private final WorkspaceUserRepository workspaceUserRepository;
     private final RoleRepository roleRepository;
