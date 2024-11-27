5eed00489758008398c819e54759b5065b310905
asharmehmood-7 <asharmehmood056@gmail.com>
2024-11-27 14:34:32 +0200

    updated comment service

--- src/main/java/org/project_management/application/services/task/CommentServiceImpl.java
+++ src/main/java/org/project_management/application/services/task/CommentServiceImpl.java
@@ -1,13 +1,18 @@
 package org.project_management.application.services.task;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.project_management.application.dto.comment.CommentCreate;
 import org.project_management.application.dto.comment.CommentMapper;
 import org.project_management.application.dto.comment.CommentUpdate;
+import org.project_management.application.exceptions.ResourceNotFoundException;
 import org.project_management.application.services.User.UserService;
+import org.project_management.domain.abstractions.AuthRepository;
 import org.project_management.domain.abstractions.CommentRepository;
+import org.project_management.domain.abstractions.TaskRepository;
 import org.project_management.domain.entities.comment.Comment;
 import org.project_management.domain.entities.task.Task;
 import org.project_management.domain.entities.user.User;
+import org.project_management.presentation.config.JwtAuthFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.List;
@@ -16,26 +21,27 @@ import java.util.UUID;
 
 public class CommentServiceImpl implements CommentService{
     private final CommentRepository commentRepository;
-    private final TaskService taskService;
-    private final UserService userService;
+    private final TaskRepository taskRepository;
+    private final JwtAuthFilter jwtAuthFilter;
+    private final HttpServletRequest request;
+    private final AuthRepository authRepository;
 
     @Autowired
-    public CommentServiceImpl(CommentRepository commentRepository, TaskService taskService, UserService userService) {
-
+    public CommentServiceImpl(CommentRepository commentRepository, TaskRepository taskRepository, JwtAuthFilter jwtAuthFilter, HttpServletRequest request, AuthRepository authRepository) {
         this.commentRepository = commentRepository;
-        this.taskService = taskService;
-        this.userService = userService;
+        this.taskRepository = taskRepository;
+        this.jwtAuthFilter = jwtAuthFilter;
+        this.request = request;
+        this.authRepository = authRepository;
     }
 
 
     @Override
     public Comment save(CommentCreate createDTO) {
-        Task task = taskService.findById(createDTO.getTaskId())
-                .orElseThrow(() -> new IllegalArgumentException("Task not found with id: " + createDTO.getTaskId()));
-
-        User user = userService.findById(createDTO.getUserId())
-                .orElseThrow(() -> new IllegalArgumentException("User not found with id: " + createDTO.getUserId()));
-
+        Task task = taskRepository.findById(createDTO.getTaskId())
+                .orElseThrow(() -> new ResourceNotFoundException("Task not found with id: " + createDTO.getTaskId()));
+        String userEmail = jwtAuthFilter.getUserEmailFromToken(request);
+        User user = authRepository.findByEmail(userEmail).orElseThrow(() -> new ResourceNotFoundException("User not found"));
         Comment comment = CommentMapper.toComment(createDTO);
         comment.setTask(task);
         comment.setUser(user);
@@ -71,7 +77,6 @@ public class CommentServiceImpl implements CommentService{
 
         return commentRepository.save(existingComment);
     }
-
 
     @Override
     public void deleteByIdAndTaskId(UUID commentId, UUID taskId) {
