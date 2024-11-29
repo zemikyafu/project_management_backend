ad486fb7e1e072b012d97e5f239b66ccb2907146
Jere Stigell <jj.stigell@gmail.com>
2024-11-29 13:47:30 +0900

    blank role and invitation tests

--- src/test/java/org/project_management/InvitationControllerIT.java
+++ src/test/java/org/project_management/InvitationControllerIT.java
@@ -1,4 +1,19 @@
 package org.project_management;
 
-public class InvitationControllerIT {
+import jakarta.transaction.Transactional;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@SpringBootTest
+@AutoConfigureMockMvc(addFilters = false)
+@ExtendWith(SpringExtension.class)
+@Transactional
+class InvitationControllerIT {
+    @Test
+    void todo() {
+        assert true;
+    }
 }
--- src/test/java/org/project_management/RoleControllerIT.java
+++ src/test/java/org/project_management/RoleControllerIT.java
@@ -1,4 +1,19 @@
 package org.project_management;
 
-public class RoleControllerIT {
+import jakarta.transaction.Transactional;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@SpringBootTest
+@AutoConfigureMockMvc(addFilters = false)
+@ExtendWith(SpringExtension.class)
+@Transactional
+class RoleControllerIT {
+    @Test
+    void todo() {
+        assert true;
+    }
 }
